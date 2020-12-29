f187079c74eea5e7785cea292ee50491b751e41b
Azhir <azhir@buzzjective.com>
2020-12-29 15:50:15 +0330

    add seed for user and it's shop

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,2 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+user = User.find_or_create_by(email: ENV['USER_EMAIL']).tap{|obj| obj.update(password: ENV['USER_PASSWORD'])}
+shop = Shop.find_or_create_by(name: ENV['SHOP_NAME']).tap{|obj| obj.update(user_id: user.id)}
